06d9ea1d728ec1b427af5ecc95937cfddf4c2b94
yyyyaaa <yaphats280396@gmail.com>
2017-01-07 04:10:46 +0000

    Revert cloudinary

--- cookit/config/initializers/shrine_storage.rb
+++ cookit/config/initializers/shrine_storage.rb
@@ -1,6 +1,5 @@
 require "shrine"
-require "cloudinary"
-require "shrine/storage/cloudinary"
+require "shrine/storage/file_system"
 
 Shrine.storages = {
   cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
